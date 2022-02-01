f66e5ce6abdf5bec46f2ec5426771e07068ae45e
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-02-01 02:36:51 -0500

    change push

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -1,3 +1,4 @@
 var today = moment();
 var x = ["8AM","9AM", "10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM","6PM"];
 $(".lead").text(today.format("HH")).html(x);
+//simple comment change for push to git
